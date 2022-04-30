89bbf0b9ebfb0be9e3e3ea175660463ce771b4be
Richard Chu <rchu1211@gmail.com>
2022-04-29 22:08:31 -0700

    second commit

--- time_app/views.py
+++ time_app/views.py
@@ -4,6 +4,6 @@ from time import gmtime, strftime
 
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+        "time": strftime("%B %d, %Y %H:%M %p", gmtime())
     }
     return render(request, 'index.html', context)
